5b76dab66057b57f6816c9deeaeaaff077bf3078
Oduvanchik43 <oduvanchikyan@gmail.com>
2023-01-25 13:27:30 +0200

    adding animation with scroll

--- js/index.js
+++ js/index.js
@@ -25,3 +25,22 @@ for (let anchor of anchors) {
 		})
 	})
 }
+// Watcher
+const items = document.querySelectorAll('[data-item]');
+const options = {
+	threshold: 0.2
+}
+
+const callback = (entries) => {
+	entries.forEach(entry => {
+		if(entry.isIntersecting) {
+			entry.target.classList.add('active');
+		}
+	});
+}
+
+const observer = new IntersectionObserver(callback, options);
+
+items.forEach(item => {
+	observer.observe(item)
+})
